e501876c28994785df7d6de76fcd8bdd46fae247
nexryai <git@nexryai.online>
2023-06-23 19:40:24 +0900

    インストール支援

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+set -e
+
+wget https://github.com/nexryai/lance-light/releases/latest/download/LanceLight-linux-amd64.zip
+unzip LanceLight-linux-amd64.zip
+rm LanceLight-linux-amd64.zip
+mv llfctl /usr/bin/
+
+curl https://raw.githubusercontent.com/nexryai/lance-light/main/systemd/lance.service > /etc/systemd/system/lance.service
+curl https://raw.githubusercontent.com/nexryai/lance-light/main/config.default.yml > /etc/lance.yml
