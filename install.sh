7157efadf4ec6c64d865bd3d927041374ceb6be4
nexryai <nexryai@sda1.net>
2023-12-16 17:21:14 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,43 @@
 
 set -e
 
-wget https://github.com/nexryai/lance-light/releases/latest/download/LanceLight-linux-amd64.zip
-unzip LanceLight-linux-amd64.zip
-rm LanceLight-linux-amd64.zip
+required_commands=("wget" "curl" "unzip" "nft")
+
+for cmd in "${required_commands[@]}"; do
+    if command -v "$cmd" > /dev/null 2>&1; then
+        echo "Checking $cmd >> OK!"
+    else
+        echo "$cmd is not installed. Please install it first."
+        exit 1
+    fi
+done
+
+ARCHITECTURE=$(uname -m)
+
+case $ARCHITECTURE in
+    x86_64)
+        ARCH="amd64"
+        ;;
+    armv7l)
+        ARCH="arm"
+        ;;
+    aarch64)
+        ARCH="arm64"
+        ;;
+    mips64)
+        ARCH="mips64"
+        ;;
+    *)
+        echo "Unsupported ARCHITECTURE >_< : $ARCHITECTURE"
+        exit 1
+        ;;
+esac
+
+ZIPNAME="LanceLight-linux-$ARCH.zip"
+
+wget "https://github.com/nexryai/lance-light/releases/latest/download/$ZIPNAME"
+unzip $ZIPNAME
+rm $ZIPNAME
 mv llfctl /usr/bin/
 
 curl https://raw.githubusercontent.com/nexryai/lance-light/main/systemd/lance.service > /etc/systemd/system/lance.service
@@ -13,4 +47,11 @@ if [ ! -f /etc/lance.yml ]; then
   curl https://raw.githubusercontent.com/nexryai/lance-light/main/config.default.yml > /etc/lance.yml
 fi
 
+# ResotreconがあるならSELinuxに怒られないようにする
+if command -v restorecon >/dev/null 2>&1; then
+    restorecon -R /usr/bin/llfctl
+fi
+
 systemctl daemon-reload
+
+echo "Lance Light has been installed successfully. enjoy! :)"
